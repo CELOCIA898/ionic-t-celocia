9d979cb0a4eb4c57f4e86c96a51654cd307986aa
CELOCIA898 <20211898@nbsc.edu.ph>
2024-04-23 08:19:06 +0800

    added a changes in tab3

---
+++ src/pages/Clickcounter.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons, IonBackButton } from '@ionic/react';
+import './Clickcounter.css';
+
+const Clickcounter: React.FC = () => {
+  const [clickCount, setClickCount] = useState(0);
+
+  const handleButtonClick = () => {
+    setClickCount(clickCount + 1);
+  };
+
+  const handleResetClick = () => {
+    setClickCount(0);
+  };
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonBackButton defaultHref="/" />
+          </IonButtons>
+          <IonTitle>Click Counter</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <div className="tab2-container">
+          <p className="tab2-click-count">Click Count: {clickCount}</p>
+          <button className="tab2-button" onClick={handleButtonClick}>Click me</button>
+          <button className="tab2-reset-button" onClick={handleResetClick}>Reset</button>
+        </div>
+      </IonContent> 
+    </IonPage>
+  );
+};
+
+export default Clickcounter;
